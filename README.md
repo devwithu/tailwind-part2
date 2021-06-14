c1c422b357a36c117ee6552a2e60f0adad91639e
devwithu <devwithu@naver.com>
2021-06-15 07:41:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# tailwind-part2
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./dist/**/*.html'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ['top', 'left'],
+    },
+
+    extend: {
+      fontFamily: { 
+        headline: ['Oswald']
+      },
+
+      colors: {
+        mainColor: '​#1E293B',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-debug-screens"),
+  ],
+}
